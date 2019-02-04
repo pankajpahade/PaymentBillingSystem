48d7cabc1970d34aa8653830a7036fef4ee3b2b5
geeta.lakhwani <geeta.lakhwani@ZIL413.perficient.com>
2019-02-04 17:46:11 +0530

    Have added college form functionality

--- src/com/payment/action/CollegeAction.java
+++ src/com/payment/action/CollegeAction.java
@@ -1,5 +1,8 @@
 package com.payment.action;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -8,12 +11,50 @@ import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
 
+import com.payment.db.DBConnection;
 import com.payment.form.CollegeForm;
 
 public class CollegeAction extends Action{
 	
-	public void display()
-	{
+
+	DBConnection connection=null;
+	@Override
+	public ActionForward execute(ActionMapping mapping, ActionForm form, HttpServletRequest req,
+			HttpServletResponse resp) throws Exception {
 		
+		connection = new DBConnection();
+		 String status = "";
+		
+		CollegeForm collegeFormBean =  (CollegeForm) form;
+		
+		String sql = "insert into collegeinfo (CName) values(?)";
+		try {
+			Connection getConnection = connection.getDbConn();
+			
+			PreparedStatement ps = getConnection.prepareStatement(sql);
+			
+			ps.setString(1, collegeFormBean.getClgName());
+			
+			
+			int i = ps.executeUpdate();
+			
+			if (i == 1) {
+				status="success";
+				
+			}
+			else
+			{
+				status = "error";
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}	
+		
+		
+	
+		
+	
+		return mapping.findForward(status);
 	}
+
 }
